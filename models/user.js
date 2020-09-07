275ff7eb3bd793f4a02004a59f5770167e104b70
New Dad <josh_panakkal3122@hotmail.com>
2020-09-06 20:16:20 -0400

    change models.walkingChallenge to models.allChallenges on line 24)

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcryptjs");
 // Creating our User model
 module.exports = function(sequelize, DataTypes) {
   const User = sequelize.define("User", {
+
     // The email cannot be null, and must be a proper email before creation
     email: {
       type: DataTypes.STRING,
@@ -18,11 +19,13 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false
     }
   });
+
   User.associate = function(models) {
-    User.hasMany(models.walkingChallenge, {
+    User.hasMany(models.allChallenges, {
       onDelete: "cascade"
     });
    }
+   
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
   User.prototype.validPassword = function(password) {
     return bcrypt.compareSync(password, this.password);

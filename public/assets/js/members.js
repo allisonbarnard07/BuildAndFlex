f979356b59649a30130deadcf8c819cd809c221e
Haymanot W <haymanotalemu21@gmail.com>
2020-09-08 18:50:43 -0400

    walking client side code

--- public/assets/js/members.js
+++ public/assets/js/members.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
     var selectchallengeType = $("#type");
     selectchallengeType.on("change", handleChallengeChange);
-    function handleChallengeChange(){
+    function handleChallengeChange(event){
+        event.preventDefault();
         let challengeType = event.target.value
         if( challengeType === "walking"){
             window.location.replace("/walking")

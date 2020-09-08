857b9acfa3aade1773c75db32dd95679f23edfd0
New Dad <josh_panakkal3122@hotmail.com>
2020-09-07 20:49:29 -0400

    update database

--- db/shema.sql
+++ db/shema.sql
@@ -4,63 +4,12 @@ CREATE DATABASE buildAndFlex_db;
 
 USE buildAndFlex_db;
 
-CREATE TABLE walkingChallenge (
-id INT NOT NULL auto_increment,
-username VARCHAR(100) NOT NULL,
-age INT NOT NULL,
-startingWeight INT DEFAULT NULL,
-endWeight INT DEFAULT NULL,
-goal VARCHAR(150) NOT NULL,
-duration INT NOT NULL,
-miles INT NOT NULL,
-steps INT,
-PRIMARY KEY(id)
-);
-
-CREATE TABLE swimChallenge (
-id INT NOT NULL auto_increment,
-username VARCHAR(80) NOT NULL,
-age INT NOT NULL,
-goal VARCHAR (150) NOT NULL,
-startingWeight INT DEFAULT NULL,
-endWeight INT DEFAULT NULL,
-duration INT NOT NULL,
-miles INT NOT NULL,
-PRIMARY KEY(id)
-);
-
-CREATE TABLE hikeChallenge (
-id INT NOT NULL auto_increment,
-username VARCHAR(100) NOT NULL,
-age INT NOT NULL,
-startingWeight INT DEFAULT NULL,
-endWeight INT DEFAULT NULL,
-goal VARCHAR(150) NOT NULL,
-duration INT NOT NULL,
-miles INT NOT NULL,
-PRIMARY KEY(id)
-);
-
-CREATE TABLE pullupChallenge (
+CREATE TABLE user (
     id INT AUTO_INCREMENT,
-    username VARCHAR (100) NOT NULL,
+    email varchar(150) not null,
+    password varchar(150) not null,
     age INT,
-    startingWeight INT,
-    endWeight INT,
-    goal varchar (150) NOT NULL,
-    duration INT NOT NULL,
-    reps INT NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE runningChallenge (
-    id INT AUTO_INCREMENT,
-    username VARCHAR (100) NOT NULL,
-    age INT,
-    startingWeight INT,
-    endWeight INT,
-    goal varchar (150) NOT NULL,
-    duration INT NOT NULL,
-    miles INT NOT NULL,
+    firstName varchar(150),
+    lastName varchar(150),
     PRIMARY KEY (id)
 )

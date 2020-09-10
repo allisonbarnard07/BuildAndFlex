63705a837c200be96fcc1e07783203cbb0304274
Justin Roose <justin.d.roose@gmail.com>
2020-09-09 20:26:59 -0400

    disregard

--- db/shema.sql
+++ db/shema.sql
@@ -4,6 +4,16 @@ CREATE DATABASE buildAndFlex_db;
 
 USE buildAndFlex_db;
 
+CREATE TABLE user (
+    id INT AUTO_INCREMENT,
+    email varchar(150) not null,
+    password varchar(150) not null,
+    age INT,
+    firstName varchar(150),
+    lastName varchar(150),
+    PRIMARY KEY (id)
+)
+
 CREATE TABLE walkingChallenge (
 id INT NOT NULL auto_increment,
 username VARCHAR(100) NOT NULL,

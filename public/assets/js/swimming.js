744cc31a5f124c41ed1c30c002769cf76e2c64a9
Justin Roose <justin.d.roose@gmail.com>
2020-09-12 15:51:10 -0400

    readme updated

--- public/assets/js/swimming.js
+++ public/assets/js/swimming.js
@@ -1,40 +1,39 @@
-$(document).ready(function() {
-    // Getting references to our form and input
-    var swimingForm = $("form.swimming-form");
-    var goalInput = $("input#swimming-goal");
-    var durationInput = $("input#swimming-duration");
-    var milesInput = $("input#swimming-miles");
-    // When the Add challenge button is clicked, we validate the the inputs.
-    swimingForm.on("submit", function(event){
-        event.preventDefault();
-        var userData = {
-            goal: goalInput.val().trim(),
-            duration: durationInput.val().trim(),
-            miles: milesInput.val().trim()
-        };
-        if (!userData.goal || !userData.duration || !userData.miles ) {
-            return;
-        }
-      // If we have a goal, miles, duration  run the addChallenge function
-        addChallenge(userData.goal, userData.duration, userData.miles);
-        goalInput.val("");
-        durationInput.val("");
-        milesInput.val("")
-    });
+$(document).ready(() => {
+  // Getting references to our form and input
+  const swimingForm = $("form.swimming-form");
+  const goalInput = $("input#swimming-goal");
+  const durationInput = $("input#swimming-duration");
+  const milesInput = $("input#swimming-miles");
+  // When the Add challenge button is clicked, we validate the the inputs.
+  swimingForm.on("submit", event => {
+    event.preventDefault();
+    const userData = {
+      goal: goalInput.val().trim(),
+      duration: durationInput.val().trim(),
+      miles: milesInput.val().trim()
+    };
+    if (!userData.goal || !userData.duration || !userData.miles) {
+      return;
+    }
+    // If we have a goal, miles, duration  run the addChallenge function
+    addChallenge(userData.goal, userData.duration, userData.miles);
+    goalInput.val("");
+    durationInput.val("");
+    milesInput.val("");
+  });
   // Does a post to the swim challenge route. If successful, we are redirected to the profile? page
-// Otherwise we log any errors
-    function addChallenge(goal, duration,miles) {
+  // Otherwise we log any errors
+  function addChallenge(goal, duration, miles) {
     $.post("/api/swimming", {
-        goal: goal,
-        duration: duration,
-        miles: miles
-    })
-        .then(function(data) {
-        window.location.replace("/profile");
+      goal: goal,
+      duration: duration,
+      miles: miles
+    }).then(data => {
+      window.location.replace("/profile");
       // If there's an error, handle it by throwing up a bootstrap alert
     });
-    };
-    $.get("/api/user_data").then(function(data) {
-        $("#id-input").val(data.id);
-    });
+  }
+  $.get("/api/user_data").then(data => {
+    $("#id-input").val(data.id);
+  });
 });
--- server.js
+++ server.js
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
 // Requiring necessary npm packages
 var express = require("express");
 var session = require("express-session");
 var hbs = require("express-handlebars");
-=======
-
-const express = require("express");
-const session = require("express-session");
->>>>>>> 6cbba172d150f8a039f3168670f51def8c05105c
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
 
@@ -16,17 +10,12 @@ const PORT = process.env.PORT || 8080;
 const db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
-<<<<<<< HEAD
 var app = express();
 // View Engine setup
 app.set("views", path.join(__dirname, "views"));
 app.engine("hbs", hbs({ extname: "hbs",defaultLayout: "main", layoutsDir: __dirname + "/views/layouts/" })); 
 app.set("view engine", "hbs");
 app.use(express.urlencoded({ extended: true }));// should it be like this or should we make it false
-=======
-const app = express();
-app.use(express.urlencoded({ extended: true }));
->>>>>>> 6cbba172d150f8a039f3168670f51def8c05105c
 app.use(express.json());
 app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
@@ -36,11 +25,6 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-<<<<<<< HEAD
-// Import routes and give the server access to them.
-require("./controllers/html-routes.js")(app);
-require("./controllers/api-routes.js")(app);
-=======
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
@@ -51,7 +35,6 @@ app.set("view engine", "handlebars");
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
->>>>>>> 6cbba172d150f8a039f3168670f51def8c05105c
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync({force:false}).then(() => {
